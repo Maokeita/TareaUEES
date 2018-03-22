ff28293d2e6cfec5ed98a10da8e6d5b4fd3bd0dc
Maokeita <35873907+Maokeita@users.noreply.github.com>
2018-03-22 10:27:36 -0600

    subida de datos

---
+++ EjercicioTarea2/ejercicio2.rb
@@ -0,0 +1,15 @@
+class Ejercicio2
+  def initialize()
+  end
+  def solucion(numero1)
+    print "la suma de los valores es", numero1 / 9.8
+  end
+end
+while condition
+  puts "\t Ejercicio 2"
+  puts "Ingresa la fuerza en Newton(kg. m/s2)"
+  numero1 = gets.chomp.to_f
+  objeto = Ejercicio2.new
+  objeto.solucion(numero1)
+end
+gets()
