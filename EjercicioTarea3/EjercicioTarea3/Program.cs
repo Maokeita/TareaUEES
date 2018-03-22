92fdb0437b982c9b5d75050cd0f48488d0e579f9
Maokeita <35873907+Maokeita@users.noreply.github.com>
2018-03-22 13:23:46 -0600

    Ejercicio3
    Subida de ejercicio 3

---
+++ EjercicioTarea3/EjercicioTarea3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioTarea3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string salir;
+            try
+            {
+                do
+                {
+                    double volt, res;
+                    double total;
+                    Console.WriteLine("Ingrese el Voltaje");
+                    volt = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Ingrese la Resistencia (Ohm)");
+                    res = double.Parse(Console.ReadLine());
+                    total = volt / res;
+                    Console.WriteLine("La corriente es: "+ total +" Ampereos");
+                    Console.WriteLine("Desea Continuar (Si, No)");
+                    salir = Console.ReadLine();
+                } while (salir == "Si");
+                Console.WriteLine("Gracias por participar");
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Solo ingrese numeros");
+                Console.ReadKey();
+                Console.WriteLine("El sistema se cerrara");
+                Console.WriteLine("Adios");
+                Console.ReadKey();
+            }
+        }
+    }
+}
